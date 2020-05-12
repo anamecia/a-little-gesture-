1ffe1882f643b6415f87ad60a4e26f491d5269bc
anamecia <anameciarobalo@gmail.com>
2020-05-12 19:01:02 +0100

    implement react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Switch>
+      </Switch>
     </div>
   );
 }
